c2277e930905bf0f9f062d7e7de6dc178ad5e295
Noah Adams <noah.adams@gmail.com>
2017-01-24 21:43:25 -0800

    initial work to illustrate escaping bug in apidoc 0.17.4

---
+++ example/example.py
@@ -0,0 +1,15 @@
+def example()
+    """
+    @api {GET} /
+    @apiName ErrorExample
+    @apiGroup Error
+
+    @apiSuccess {String} html The html
+
+    @apiSuccessExample {json} Success-Response:
+    HTTP/1.1 200 OK
+    {
+        "html": "<iframe></iframe>"
+    }
+    """
+    pass
